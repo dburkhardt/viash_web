7981e6bcc6a1076f70b56282f957de345ff66d0d
Robrecht Cannoodt <rcannood@gmail.com>
2021-05-28 15:09:23 +0200

    add helper files

---
+++ src/build_rmd/script.R
@@ -0,0 +1,35 @@
+setwd(par$viash_docs)
+
+# start docker daemon
+system("dockerd", ignore.stdout = TRUE, ignore.stderr = TRUE, wait = FALSE)
+
+# Rmd files under the root directory
+rmds <- c(
+  list.files("content_raw/", '[.]Rmd$', recursive = TRUE, full.names = TRUE)
+)
+
+# get corresponding mds
+mds <- gsub("content_raw/", "content/", blogdown:::with_ext(rmds, '.md'))
+
+
+for (i in seq_along(rmds)) {
+  rmd <- rmds[[i]]
+  md <- mds[[i]]
+
+  # if output is not older than input, skip the compilation
+  if (blogdown:::require_rebuild(md, rmd)) {
+    message('* knitting ', rmd)
+
+    rmarkdown::render(
+      input = rmd,
+      output_file = basename(md),
+      output_dir = dirname(md),
+      output_format = rmarkdown::md_document(
+        variant = "gfm",
+        preserve_yaml = TRUE
+      )
+    )
+  }
+}
+
+message('DONE!')
