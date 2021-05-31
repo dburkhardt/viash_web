0df52e8152f3ea202a5d146206938e04a12c8ed3
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2021-05-31 09:22:12 +0200

    Removed package.json dependancies + python creating component WIP

--- static/examples/md_url_checker_py/script.py
+++ static/examples/md_url_checker_py/script.py
@@ -2,6 +2,8 @@
 # packages: python3-lxml
 # pip: markdown, lxml, requests
 
+### 1 ###
+
 ## VIASH START
 
 par = {
@@ -16,11 +18,15 @@ import markdown
 from lxml import etree
 import requests
 
+### 2 ###
+
+# Open markdown file, convert it to html and parse it to an ElementTree
 with open(par["inputfile"], encoding="utf8") as file:
     md = file.read().replace('\n', '')
 
 doc = etree.fromstring(markdown.markdown(md))
 
+# Create and clear output file
 output_file = open(par['output'], 'a')
 output_file.truncate(0)
 
@@ -28,10 +34,16 @@ amount_of_urls = len(doc.xpath('//a'))
 amount_of_errors = 0
 expected_code = 200
 
+### 3 ###
+
+# Iterate over all hyperlinks and check each URL
 for index, link in enumerate(doc.xpath('//a')):
     title = link.text
     url = link.get('href')
 
+    ### 4 ###
+
+    # If an URL doesn't start with 'http', add the domain before it
     if not url.startswith('http'):
         url = par["domain"] + url
 
@@ -40,9 +52,13 @@ for index, link in enumerate(doc.xpath('//a')):
     output_file.write("Link name: " + title + "\n")
     output_file.write("URL: " + url + "\n")
 
+    ### 5 ###
+
     try:
+        # Do a HEAD request and get the status code from the last response after following any redirects
         req = requests.head(url, allow_redirects=True)
 
+        # Check if status code contains 200, which means the connection was succesful
         if req.status_code == 200:
             print('OK')
             output_file.write("Status: OK, can be reached.\n")
